92f5b4410c50003fd924c4513288e917e3dd79cb
norths233 <norths233@gmail.com>
2022-02-21 01:56:40 +0700

    nn

--- src/app/mycomp/directivetest/directivetest.component.ts
+++ src/app/mycomp/directivetest/directivetest.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivetestComponent implements OnInit {
   students = [
-      {name: 'Mickey' , email: 'mickey@uva.edu', major: 'CS', year: 'second'},
-      {name: 'Minney' , email: 'minney@uva.edu', major: 'CS', year: 'third'},
-      {name: 'duh' , email: 'duh@uva.edu', major: 'SWE', year: 'third'},
-      {name: 'huh' , email: 'huh@uva.edu', major: 'SWE', year: 'second'}
+      {name: 'north' , email: 'north@uva.edu', major: 'CS', year: 'second'},
+      {name: 'nqk' , email: 'nqk@uva.edu', major: 'CS', year: 'third'},
+      {name: 'nonny' , email: 'nonny@uva.edu', major: 'SWE', year: 'third'},
+      {name: 'nan' , email: 'nan@uva.edu', major: 'SWE', year: 'second'}
     ];
   constructor(){}
   ngOnInit(): void {
